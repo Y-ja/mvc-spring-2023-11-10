6d80b68b7cfeff105275510be26c042433e2b2ec
20230720 <ghkdwo34@naver.com>
2023-11-25 11:21:30 +0900

    jsp기초 다이어리 폼 홈페이지 만들기(기초)

---
+++ src/main/java/com/spring/mvc/chap05/entity/BoardController.java
@@ -0,0 +1,14 @@
+package com.spring.mvc.chap05.entity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/borad")
+public class BoardController {
+    // 1. 목록 조회 요청 (/board/list : GET)
+    // 2. 글쓰기 화면요청 (/board/write : GET)
+    // 3. 글쓰기 등록요청 (/board/write : POST)
+    // 4. 글 삭제 요청 (/board/delete : GET)
+    // 5. 글 상세보기 요청 (/board/detail : GET)
+}
